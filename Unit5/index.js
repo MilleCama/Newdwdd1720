888865173cbf70f0dd98915f11b775408395fc55
MilleCama <martille349@gmail.com>
2024-03-27 14:55:25 -0600

    n

--- Unit5/index.js
+++ Unit5/index.js
@@ -3,6 +3,7 @@ let player = {
     name: "Per",
     chips: 200
 }
+
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -20,11 +21,11 @@ playerEl.textContent = player.name + ": $" + player.chips
 
 
 function getRandomCard() {
-    let randomNumber = Math.floor (Math.random()*13) + 1
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
     if (randomNumber > 10) {
         return 10
     } else if (randomNumber === 1) {
-        return 10
+        return 11
     } else {
         return randomNumber
     }
@@ -59,7 +60,7 @@ function renderGame () {
 }
 
 function newCard() {
-    if (isAlive === true && hasBlackJack === false) {
+    if (isAlive === true && hasBlackJack === true) {
         let card = getRandomCard()
         sum += card
         cards.push(card)
