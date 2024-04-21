878f084ff4ff4c1cc9109c03a5bcc0fbd2b16d6a
MilleCama <martille349@gmail.com>
2024-04-21 05:36:21 -0600

    oof

--- Unit10/js/scripts.js
+++ Unit10/js/scripts.js
@@ -10,36 +10,45 @@ const btnAll = document.createElement('button')
 btnAll.textContent = "All People"
 btnAll.addEventListener('click', () => displayPeople(allSenators));
 
+
 //event listener and filter for the women
 const btnFemale = document.createElement('button');
 btnFemale.textContent = 'Women'
 btnFemale.addEventListener('click', () => {
-    const femaleSenators = allSenators.filter (person => person.gender === 'female')
+    const femaleSenators = allSenators.filter (person => person.gender === 'F')
     displayPeople(femaleSenators)})
+
+
 
 
 //event listener and filter for the men
  const btnMale = document.createElement('button');
     btnMale.textContent = 'Men'
     btnMale.addEventListener('click', () => {
-        const maleSenators = allSenators.filter (person => person.gender === 'male')
+        const maleSenators = allSenators.filter (person => person.gender === 'M')
         displayPeople(maleSenators)})
+
+
 
 
 //event listener and filter for the republican
 const btnRepublican = document.createElement('button');
 btnRepublican.textContent = 'Republican'
 btnRepublican.addEventListener('click', () => {
-    const republicanSenators = allSenators.filter (person => person.party === 'republican')
+    const republicanSenators = allSenators.filter (person => person.party === 'R')
     displayPeople(republicanSenators)})
+
+
 
 
 //event listener and filter for the democrat
 const btnDemocrat = document.createElement('button');
 btnDemocrat.textContent = 'Democrat'
 btnDemocrat.addEventListener('click', () => {
-    const democratSenators = allSenators.filter (person=> person.party === 'democrat')
+    const democratSenators = allSenators.filter (person=> person.party === 'D')
     displayPeople(democratSenators)})
+
+
 
 
 // add buttons to page
@@ -49,21 +58,47 @@ myNavigation.appendChild(btnMale)
 myNavigation.appendChild(btnRepublican)
 myNavigation.appendChild(btnDemocrat)
 
-
 // loop through all the people
 function displayPeople (x) {
+    myParent.textContent = ""
     x.forEach(person => {
         const myFigure = document.createElement('figure')
 
         const myImage = document.createElement('img')
-
+        console.log(person.govtrack_id)
+        const explodedArray = person.govtrack_id.split('/')
+        console.log(explodedArray)
+        const charNumber = explodedArray[0]
+        myImage.src=`//www.govtrack.us/static/legislator-photos/${charNumber}.jpeg`
+        myImage.alt = person.first_name
+        const myphotoSize = "-100px.jpeg"
         const myCaption = document.createElement('figcaption')
+        myCaption.textContent = person.first_name 
+        
+        
 
 //assemble the parts
 myFigure.appendChild(myImage)
+myFigure.appendChild(myCaption)
+myParent.appendChild(myFigure)
 
 
-
+console.log(person.gender)
+switch (person.gender) {
+    case "F":
+    myFigure.className = "female"
+    break;
+    case "M":
+    myFigure.className = "male"
+}
+console.log(person.party)
+switch (person.party) {
+    case "R":
+    myFigure.className = "republican"
+    break;
+    case "D":
+    myFigure.className = "democrat"
+}
 
     }// end of fat arrow
 
